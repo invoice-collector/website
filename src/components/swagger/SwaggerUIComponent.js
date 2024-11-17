fee9c7e575950c071c61736b6549ecf953ebdb69
LucBerge <lucas.bergeron@outlook.fr>
2024-11-17 16:44:32 +0100

    Update url

--- src/components/swagger/SwaggerUIComponent.js
+++ src/components/swagger/SwaggerUIComponent.js
@@ -4,6 +4,6 @@ import 'swagger-ui-react/swagger-ui.css';
 
 export default function SwaggerUIComponent() {
   return (
-    <SwaggerUI url="https://raw.githubusercontent.com/InvoiceCollector/website/refs/heads/master/docs/openapi/swagger.yaml" /> // Replace with the path to your swagger.yaml file
+    <SwaggerUI url="https://raw.githubusercontent.com/InvoiceCollector/website/refs/heads/master/static/openapi.yaml" />
   );
 };
