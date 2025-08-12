cdd909e78969cfa734e44413a8d23241f3735ecf
LucBerge <lucas.bergeron@outlook.fr>
2025-08-12 17:40:38 +0200

    Remove useless imports

--- src/pages/pricing/index.js
+++ src/pages/pricing/index.js
@@ -1,28 +1,7 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import PricingpageFeatures from '@site/src/components/PricingpageFeatures';
 import { PricingPage } from '../../components/PricingPage';
-
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
 import './index.module.css';
-
-function PricingpageHeader() {
-  return (
-    <header className={clsx('hero', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          Pricing
-        </Heading>
-        <p className="hero__subtitle">
-          Invoice-Collector is for Free for small companies in self-hosted.
-        </p>
-      </div>
-    </header>
-  );
-}
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
