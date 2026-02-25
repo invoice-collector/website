b802a9e638194efa6a04df6b52c71e61da25d0a2
LucBerge <lucas.bergeron@outlook.fr>
2026-02-25 13:43:52 +0100

    Update email type to text

--- src/components/LandingPage/ERP_fr/index.js
+++ src/components/LandingPage/ERP_fr/index.js
@@ -829,7 +829,7 @@ export default function LandingPageFr() {
                         
                         <div>
                             <label class="block text-sm font-medium text-gray-700 mb-1">Email professionnel</label>
-                            <input type="email" name="email" required class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"></input>
+                            <input type="text" name="email" required class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"></input>
                         </div>
                         
                         <div>
