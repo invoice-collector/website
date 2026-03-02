2481684a82fb36513832e7c29c149311bffb93c9
LucBerge <lucas.bergeron@outlook.fr>
2026-03-02 18:55:31 +0100

    Fix bug - Redirection

---
+++ src/pages/fr/index.js
@@ -0,0 +1,5 @@
+import LandingPage from '@site/src/components/LandingPage/Accountant';
+
+export default function Home() {
+  return <LandingPage />;
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,15 @@
 import { useEffect } from 'react';
-import { useHistory } from '@docusaurus/router';
 import LandingPage from '@site/src/components/LandingPage/Accountant';
 
 export default function Home() {
   const isDev = process.env.NODE_ENV === 'development';
   const isFrench = typeof navigator !== 'undefined' && navigator.language && navigator.language.startsWith('fr');
-  const history = useHistory();
   console.log('isDev', isDev);
   console.log('isFrench', isFrench);
 
   useEffect(() => {
     if (!isDev && isFrench) {
-      history.push('/fr/');
+      window.location.replace('/fr/');
     }
   }, [isFrench]);
   return <LandingPage />;
