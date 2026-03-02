936df994a310d9a20258e1939edaf31185fa04bb
LucBerge <lucas.bergeron@outlook.fr>
2026-03-02 18:30:25 +0100

    Redirect to /fr/ if french

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
+import { useHistory } from '@docusaurus/router';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import LandingPageEn from '@site/src/components/LandingPage/Accountant/en';
 import LandingPageFr from '@site/src/components/LandingPage/Accountant/fr';
 
 export default function Home() {
-  const isFrench = typeof navigator !== 'undefined' && navigator.language && navigator.language.startsWith('fr');
-  return isFrench ? <LandingPageEn /> : <LandingPageEn />;
+  const {i18n: {currentLocale}} = useDocusaurusContext();
+  const isFrench = currentLocale && currentLocale.startsWith('fr');
+  const history = useHistory();
+  console.log('currentLocale', currentLocale);
+  console.log('isFrench', isFrench);
+
+  useEffect(() => {
+    if (isFrench) {
+      history.push('/fr/');
+    }
+  }, [isFrench]);
+
+  return <LandingPageEn />;
 }
